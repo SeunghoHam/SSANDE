b1197954b5caf6f39c98152e46b39a49bf6e728f
HAM <71740147+SeunghoHam@users.noreply.github.com>
2025-08-06 20:02:27 +0900

    프로젝트 생성

---
+++ Source/Sande/Sande.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sande.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sande, "Sande" );
+
+DEFINE_LOG_CATEGORY(LogSande)
+ 
